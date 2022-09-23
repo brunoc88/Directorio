2424597a6a0b024131318823242887328e139e3c
brunoc88 <brunocerutti88@gmail.com>
2022-09-22 22:45:40 -0300

    Initial commit

---
+++ src/directorio/Directorio.java
@@ -0,0 +1,133 @@
+package directorio;
+
+
+import directorio.Cliente;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Directorio {
+
+    private Cliente cliente;
+
+    private Long telefono;
+
+    private HashMap<Long,Cliente>directorio;
+
+    public Directorio() {
+        this.directorio = new HashMap();
+    }
+
+    public Directorio(Cliente cliente, Long telefono, HashMap<Long, Cliente> directorio) {
+        this.cliente = cliente;
+        this.telefono = telefono;
+        this.directorio = directorio;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public HashMap<Long, Cliente> getDirectorio() {
+        return directorio;
+    }
+
+    public void setDirectorio(HashMap<Long, Cliente> directorio) {
+        this.directorio = directorio;
+    }
+ 
+    public Cliente agregarCliente() {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n"); 
+        System.out.println("Ingrese los datos del cliente:");
+        Cliente c1 = new Cliente();
+        System.out.println("DNI:");
+        c1.setDni(leer.nextLong());
+        System.out.println("Ingrese nombre:");
+        c1.setNombre(leer.next());
+        System.out.println("Ingrese apellido:");
+        c1.setApellido(leer.next());
+        System.out.println("Ingrese Ciudad");
+        c1.setCiudad(leer.next());
+        System.out.println("Ingrese Direccion:");
+        c1.setDireccion(leer.next());
+        telefono = Math.round(Math.random()*1000)+1;
+        directorio.put(telefono, c1);
+        return c1;
+    }
+
+    public Cliente buscarCliente(Long telefono) {
+        int c = 0;
+        
+        for (Map.Entry<Long, Cliente> entry : directorio.entrySet()) {
+            Long key = entry.getKey();
+            Cliente value = entry.getValue();
+            if (key.equals(telefono)) {
+                c++;
+                System.out.println(cliente.toString());
+                return cliente;  
+               
+            }else{
+                System.out.println("El cliente no se encuentra en el directorio");
+                cliente = null;
+                
+            }
+        }
+        return cliente;
+    }
+
+    public Long buscarTelefono(String apellido) {
+        for (Map.Entry<Long, Cliente> entry : directorio.entrySet()) {
+            Long key = entry.getKey();
+            Cliente value = entry.getValue();
+            if (value.getApellido().equalsIgnoreCase(apellido)) {
+                System.out.println("telefono =" + " Cliente ="+cliente);
+                return telefono;
+            }else{
+                System.out.println("No se encontro el apellido");
+                telefono = null;
+                
+            }
+        }
+        return telefono;
+    }
+
+    public HashMap buscarClientes(String ciudad) {
+        for (Map.Entry<Long, Cliente> entry : directorio.entrySet()) {
+            Long key = entry.getKey();
+            Cliente value = entry.getValue();
+            if (value.getCiudad().equalsIgnoreCase(ciudad)) {
+                System.out.println("Directorio " + directorio);
+                return directorio;
+            }else{
+                System.out.println("No se encontro coincidencia");
+                directorio = null;
+                
+            }
+            
+        }
+        return directorio;
+    }
+
+    public void borrarClientes(Long telefono) {
+       directorio.remove(telefono);
+    }
+
+    @Override
+    public String toString() {
+        return "Directorio{" + "cliente=" + cliente + ", telefono=" + telefono + ", directorio=" + directorio + '}';
+    }
+    
+}
---
+++ src/directorio/Main.java
@@ -0,0 +1,52 @@
+
+package directorio;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n"); 
+        Directorio directorio = new Directorio();
+        String seleccion = "";
+        do {
+            System.out.println("");
+            System.out.println("Menu.");
+            System.out.println("1.Agregar cliente");
+            System.out.println("2.Buscar Cliente");
+            System.out.println("3.Buscar Telefono");
+            System.out.println("4.Buscar clientes");
+            System.out.println("5.Borrar cliente");
+            System.out.println("6.Salir");
+            int option = leer.nextInt();
+            switch (option) {
+                case 1:
+                    directorio.agregarCliente();
+                    break;
+                case 2:
+                    System.out.println(directorio.toString());
+                    System.out.println("Ingrese el telefono de la persona que desea buscar");
+                    directorio.buscarCliente(leer.nextLong());
+                    break;
+                case 3:
+                    System.out.println("Ingrese el apellido de la persona");
+                    directorio.buscarTelefono(leer.next());
+                    break;
+                case 4:
+                    System.out.println("Ingrese la ciudad de la persona");
+                    directorio.buscarClientes(leer.next());
+                    break;
+                case 5:
+                    System.out.println("ingrese el numero de telefono para borrar");
+                    directorio.borrarClientes(leer.nextLong());
+                    break;
+                case 6:
+                    System.out.println("Realmente desea salir(s/n)");
+                    seleccion = leer.next();
+            }
+        } while (!seleccion.equalsIgnoreCase("s"));
+
+    }
+    
+}
